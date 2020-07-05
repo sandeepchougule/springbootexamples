0471a19081f4678d27edb0348bdebc4952074522
Sandeep Chougule <sandeepchougule45@gmail.com>
2020-07-05 08:46:28 +0530

    Added example bar graph

--- src/main/java/com/sandeep/example/sandeep/MainApplication.java
+++ src/main/java/com/sandeep/example/sandeep/MainApplication.java
@@ -1,16 +1,18 @@
 package com.sandeep.example.sandeep;
 
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-
 public class MainApplication {
 
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(MainApplication.class, args);
 	}
 
